977734309ccb91ce8becce5c36e2892763c08450
jsvivical <jsvivical@gmail.com>
2021-07-09 16:56:54 +0900

    20210709 16:56 go를 활용한 머신러닝 json파일 이용, sql이용 공부

---
+++ SQL유형 데이터베이스/SQL데이터베이스에 연결하기.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	pgURL := os.Getenv("PGURL")
+	if pgURL == "" {
+		log.Fatal("PGURL empty")
+	}
+	//데이터베이스값을 연다
+	// database/sql을 위한 postgres 드라이버를 지정한다.
+	db, err := sql.Open("postgres", pgURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//데이터베이스에 성공적으로 연결됐는지 확인하기 위해
+	//Ping메소드를 사용할 수 있음
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ json파일이용/json파싱.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const citiBikeURL = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json"
+
+//stationData는 citiBikeURL로부터 반환된 JSON 문서의 구문을 분석하는 데 사용된다.
+
+type stationData struct {
+	LastUpdated int `json:"last_updated"`
+	TTL         int `json:"ttl"`
+	Data        struct {
+		Stations []station `json:"stations"`
+	} `json:"data"`
+}
+
+//유의사항 `json:""` 에서 : 의 이전 이후로 공백을 넣으면 안됨
+//station은 stationData 안의 각 station 문서의 구문을 분석하는데 사용된다.
+type station struct {
+	ID                string `json:"station_id"`
+	NumBikesAvailable int    `json:"num_bikes_available"`
+	NumBikesDisabled  int    `json:"num_bikes_disabled"`
+	NumDocksAvailable int    `json:"num_docks_available"`
+	NumDocksDisable   int    `json:"num_docks_disabled"`
+	IsInstalled       int    `json:"is_installed"`
+	IsRenting         int    `json:"is_renting"`
+	IsReturning       int    `json:"is_returning"`
+	LastReported      int    `json:"last_reported"`
+	HasAvailableKeys  bool   `jso:"eighted_has_available_keys"`
+}
+
+/*
+유의사항
+밑줄이 있는 필드를 피함으로써 Go의 관용적 사용법을 따랐지만
+JSON 데이터에서 예상되는 필드에 따라 구조체 필드에 레이븡ㄹ을 추가하기 위해 json 구조체 태그를 사용했다.
+
+*/
+
+func main() {
+	//URL로부터 JSON의 응답을 얻는다.
+	response, err := http.Get(citiBikeURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	//응답의 Body를 []byte로 읽는다.
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//stationData 유형의 변수를 선언한다.
+
+	var sd stationData
+	//stationData 변수로 JSON데이터를 읽는다
+	if err := json.Unmarshal(body, &sd); err != nil {
+		log.Fatal(err)
+	}
+	//첫번째 정류장 정보를 출력한다.
+
+	for _, v := range sd.Data.Stations {
+		fmt.Println(v)
+	}
+
+}
